a98840937dacf288ef0de703e2d614ed8054f26e
Laszlo <uray077@gmail.com>
2021-03-01 11:11:39 +0000

    update on budget app

--- budget_app.py
+++ budget_app.py
@@ -45,7 +45,56 @@ class Category:
 
 
 def create_spend_chart(categories):
-    pass
+  spent_dict = {}
+  for i in categories:
+    s = 0 
+    for j in i.ledger:
+      if j['amount'] < 0 :
+        s+= abs(j['amount'])
+    spent_dict[i.name] = round(s,2)
+  total = sum(spent_dict.values())
+  percent_dict = {}
+  for k in spent_dict.keys():
+    percent_dict[k] = int(round(spent_dict[k]/total,2)*100)
+  output = 'Percentage spent by category\n'
+  for i in range(100,-10,-10):
+    output += f'{i}'.rjust(3) + '| '
+    for percent in percent_dict.values():
+      if percent >= i:
+        output+= 'o  '
+      else:
+        output+= '   '
+    output += '\n' 
+  output += ' '*4+'-'*(len(percent_dict.values())*3+1)
+  output += '\n     '
+  dict_key_list = list(percent_dict.keys())
+  max_len_category = max([len(i) for i in dict_key_list])
+  
+  for i in range(max_len_category):
+    
+    for name in dict_key_list:
+      if len(name)>i:
+        output+= name[i] +'  '
+      else:
+        output+= '   '
+    if i < max_len_category-1:
+      output+='\n     '
+    
+  return output
+
+food = Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdraw(10.15, "groceries")
+food.withdraw(15.89, "restaurant and more food for dessert")
+print(food.get_balance())
+clothing = Category("Clothing")
+food.transfer(50, clothing)
+clothing.withdraw(25.55)
+clothing.withdraw(100)
+auto = Category("Auto")
+auto.deposit(1000, "initial deposit")
+auto.withdraw(15)
+
 
 '''
     Assignment
