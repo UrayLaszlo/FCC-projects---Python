8b1332174a70b7a2ccc8e3cfd1067937fe12a13c
Laszlo <uray077@gmail.com>
2021-03-03 18:11:56 +0000

    probability calculator

--- probability_calculator.py
+++ probability_calculator.py
@@ -7,29 +7,42 @@ class Hat:
         self.args = args
         self.contents = [[k]*v for k,v in args.items()] # Add incoming dictionary to list
         self.contents = [x for y in self.contents for x in y] # Turn list of lists to list
-
+    '''
     def draw(self, amount):
         self.amount = amount
         rand_draw_list = []
-        if amount > len(self.contents):
+        if amount >= len(self.contents):
             return self.contents
         else:
-            return rand_draw_list.append(random.sample(self.contents, amount)) # Randomly select items from original list and append them to new list
-
-
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    balls_drawn = []
-    balls_drawn.append(random.sample(hat, num_balls_drawn))
-    count = 0
+             rand_draw_list.append(random.sample(self.contents, amount)) # Randomly select items from original list and append them to new list
+        rand_draw_list = [x for y in rand_draw_list for x in y]
+        return rand_draw_list
+    '''
+    def draw(self, amount):
+        rand_draw_list = []
+        if amount >= len(self.contents):
+            return self.contents
+        for i in range(amount):
+            drawn_balls = self.contents.pop(random.randrange(len(self.contents)))
+            rand_draw_list.append(drawn_balls)
+        return rand_draw_list
     
-    return 
-        
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    list_expected_balls = [[k]*v for k,v in expected_balls.items()] # Add incoming dictionary to list
+    list_expected_balls = [x for y in list_expected_balls for x in y] # Turn list of lists to list
+    count = 0
 
+    for i in range(num_experiments):
+        copy_of_hat = copy.deepcopy(hat)
+        drawn_list = copy_of_hat.draw(num_balls_drawn)
+        if drawn_list == list_expected_balls:
+            count += 1
+    
+    return count/num_experiments
 
 hat1 = Hat(yellow=3, blue=2, green=4)
-hat1.draw(5)
-experiment(Hat, {"yellow":2,"green":1}, 5, 2000)
-#hat2 = Hat(red=5, orange=4)
+#hat1.draw(5)
+experiment(hat=Hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000)#hat2 = Hat(red=5, orange=4)
 #hat2.draw(5)
 #hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
 #hat3.draw(7)
